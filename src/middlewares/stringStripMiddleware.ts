a50cb5d501c1b23fa6e13cf71725110efc4c9734
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-09-07 01:03:26 -0300

    created template.

---
+++ src/middlewares/stringStripMiddleware.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import { stripHtml } from "string-strip-html";
+
+export default function clearData(req: Request, res: Response, next: NextFunction) {
+  const data = [req.headers, req.params, req.query, req.body];
+  const output = [data];
+  for(let i = 0; i < data.length; i++) {
+    for (const param in data[i]) {
+      if (typeof output[i][param] === "string") {
+        output[i][param] = stripHtml(data[i][param]).result.trim();
+      }
+    }
+  }
+  next();
+};
