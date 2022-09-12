69b599f102da1634a1a6e6cca274b7001c6688d1
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-09-12 08:54:27 -0300

    refactor: changed type of env variable.

--- dist/database/database.js
+++
@@ -1,3 +0,0 @@
-import pkg from "@prisma/client";
-var PrismaClient = pkg.PrismaClient;
-export var prisma = new PrismaClient();
--- dist/utils/dataUtils.js
+++
@@ -1,7 +0,0 @@
-import { passwordEncrypt } from "./encryptionUtils.js";
-export function passwordFormat(userData) {
-    var bcryptPassword = passwordEncrypt(userData.password);
-    userData.password = bcryptPassword;
-    delete userData.repeatPassword;
-    return;
-}
